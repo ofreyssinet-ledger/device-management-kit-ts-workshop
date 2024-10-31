a31de2de4bbfae82a43d1ae781b9829bdbfce0a0
ofreyssinet-ledger <olivier.freyssinet@ledger.fr>
2024-10-31 18:10:59 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from 'react';
-import { firstValueFrom, lastValueFrom } from 'rxjs';
+import { useState } from 'react';
+import { firstValueFrom } from 'rxjs';
 import './App.css';
 import {
   DeviceActionStatus,
   DeviceSdkBuilder,
+  DeviceStatus,
   type DeviceSessionId,
 } from '@ledgerhq/device-management-kit';
 import {
   GetAddressDAOutput,
-  GetAddressDAReturnType,
-  KeyringEth,
   KeyringEthBuilder,
 } from '@ledgerhq/device-signer-kit-ethereum';
+import { SignTransactionDAOutput } from '@ledgerhq/device-signer-kit-ethereum/lib/cjs/index.js';
+import { ethers } from "ethers";
 import { useDeviceSessionState } from './helpers';
 
 function App() {
@@ -20,8 +21,17 @@ function App() {
 
   const [deviceSessionId, setSessionId] = useState<DeviceSessionId>();
   const [connectionError, setConnectionError] = useState<unknown>();
-  const [ethereumAddress, setEthereumAddress] = useState<string>();
+  const [derivationPath, setDerivationPath] = useState("44'/60'/0'/0");
+  const [transaction, setTransaction] = useState("0x02f87101831863388085042d5376f6825208944675c7e5baafbffbca748158becba61ef3b0a26387f4c28a5744191880c080a056dcc0364cdc4e395879d4f647c4e5d6b6112ace65605ecbf7d329b67b2006f2a023def803117565f65f763d2c5d18281ad2ef0ab4335c45da691266b23bc5d814");
+  const [getAddressOutput, setGetAddressOutput] = useState<GetAddressDAOutput>();
   const [getAddressError, setGetAddressError] = useState<unknown>();
+  const [getAddressState, setGetAddressState] = useState<unknown>();
+  const [getAddressLoading, setGetAddressLoading] = useState<boolean>(false);
+
+  const [signTransactionOutput, setSignTransactionOutput] = useState<SignTransactionDAOutput>();
+  const [signTransactionError, setSignTransactionError] = useState<unknown>();
+  const [signTransactionState, setSignTransactionState] = useState<unknown>();
+  const [signTransactionLoading, setSignTransactionLoading] = useState<boolean>(false);
 
   const onClickDiscoverDevices = async () => {
     try {
@@ -47,27 +57,62 @@ function App() {
 
   const onClickGetEthereumAddress = async () => {
     // Workshop TODO 5: implement the getAddress using the Ethereum Keyring
-    if (!keyringEth) return;
-    setEthereumAddress(undefined);
+    if (!keyringEth || !derivationPath) return;
+    setGetAddressOutput(undefined);
     setGetAddressError(undefined);
-    const derivationPath = "44'/60'/0'/0";
-    const getAddressResult = await lastValueFrom(
-      keyringEth.getAddress(derivationPath).observable
-    );
-    console.log('getAddressResult', getAddressResult);
-    if (getAddressResult.status !== DeviceActionStatus.Completed) {
-      setGetAddressError(getAddressResult);
-    } else {
-      setEthereumAddress(getAddressResult.output.address);
-    }
+    setGetAddressState(undefined)
+    setGetAddressLoading(true);
+    keyringEth.getAddress(derivationPath).observable.subscribe({
+      next: (getAddressDAState) => {
+        switch(getAddressDAState.status) {
+          case DeviceActionStatus.Completed:
+            setGetAddressOutput(getAddressDAState.output);
+            break;
+          case DeviceActionStatus.Error:
+            setGetAddressError(getAddressDAState.error);
+            break;
+          default:
+            break;
+        }
+        setGetAddressState(getAddressDAState);
+      },
+      complete: () => setGetAddressLoading(false)
+    });
   };
+
+  const onClickSignTransaction = async () => {
+    // Workshop TODO 6: implement the signTransaction using the Ethereum keyring
+    if (!keyringEth || !derivationPath || !transaction) return;
+    setSignTransactionOutput(undefined);
+    setSignTransactionError(undefined);
+    setSignTransactionState(undefined)
+    setSignTransactionLoading(true);
+    keyringEth.signTransaction(derivationPath, ethers.Transaction.from(transaction)).observable.subscribe({
+      next: (signTransactionDAState) => {
+        switch(signTransactionDAState.status) {
+          case DeviceActionStatus.Completed:
+            setSignTransactionOutput(signTransactionDAState.output);
+            break;
+          case DeviceActionStatus.Error:
+            setGetAddressError(signTransactionDAState.error);
+            break;
+          default:
+            break;
+        }
+        setSignTransactionState(signTransactionDAState);
+      },
+      complete: () => setSignTransactionLoading(false)
+    });
+  }
 
   const deviceSessionState = useDeviceSessionState(sdk, deviceSessionId);
 
+  const buttonsDisabled = getAddressLoading || !deviceSessionId || deviceSessionState?.deviceStatus !== DeviceStatus.CONNECTED
+
   return (
-    <div className="card">
+    <div className="card" style={{width: "500px"}}>
       <button onClick={onClickDiscoverDevices}>
-        Discover & connect a device
+        Discover & connect a device (USB)
       </button>
       {connectionError ? (
         <pre>
@@ -77,24 +122,62 @@ function App() {
       ) : deviceSessionId ? (
         <div>
           <p>Connected! SessionId: {deviceSessionId}</p>
-          <p>Session status: {deviceSessionState?.deviceStatus ?? 'loading'}</p>
+          <p>Device Session status: {deviceSessionState?.deviceStatus ?? 'loading'}</p>
         </div>
       ) : (
-        <p>'No active session. First discover and connect a device.'</p>
+        <p>No active session. First discover and connect via USB a Ledger device.</p>
       )}
-      <button disabled={!deviceSessionId} onClick={onClickGetEthereumAddress}>
-        Get Ethereum address
-      </button>
+      <div style={{height: 1, width: "100%", backgroundColor: "grey", margin: "20px 0px"}} />
+      <div style={{display: "flex", flexDirection: "column", rowGap: 20, alignItems: "center"}}>
+        <form>
+          <label>
+            Derivation path:{" "}
+          </label>
+          <input disabled={buttonsDisabled} value={derivationPath} onChange={(e) => setDerivationPath(e.target.value)} />
+        </form>
+        <button disabled={buttonsDisabled} onClick={onClickGetEthereumAddress}>
+          Get Ethereum address {getAddressLoading ? "(loading)" : ""}
+        </button>
+      </div>
       {getAddressError ? (
         <>
           <p>Get address error:</p>
           <pre>{JSON.stringify(getAddressError, null, 2)}</pre>
         </>
       ) : (
+        <div style={{overflow: "scroll", textAlign: "start"}}>
+          <p>GetAddressDeviceAction state:</p>
+          <pre>{getAddressState ? JSON.stringify(getAddressState, null, 2) : "undefined"}</pre>
+        </div>
+      )}
+      <div style={{height: 1, width: "100%", backgroundColor: "grey", margin: "20px 0px"}} />
+      <div style={{display: "flex", flexDirection: "column", rowGap: 20, alignItems: "center"}}>
+        <form>
+          <label>
+            Derivation path:{" "}
+          </label>
+          <input disabled={buttonsDisabled} value={derivationPath} onChange={(e) => setDerivationPath(e.target.value)} />
+        </form>
+        <form>
+          <label>
+            Transaction:{" "}
+          </label>
+          <input disabled={buttonsDisabled} value={transaction} onChange={(e) => setTransaction(e.target.value)} />
+        </form>
+        <button disabled={buttonsDisabled} onClick={onClickSignTransaction}>
+          Sign transaction {signTransactionLoading ? "(loading)" : ""}
+        </button>
+      </div>
+      {signTransactionError ? (
         <>
-          <p>Ethereum address:</p>
-          <pre>{ethereumAddress ?? 'undefined'}</pre>
+          <p>Sign transaction error:</p>
+          <pre>{JSON.stringify(signTransactionError, null, 2)}</pre>
         </>
+      ) : (
+        <div style={{overflow: "scroll", textAlign: "start"}}>
+          <p>SignTransactionDeviceAction state:</p>
+          <pre>{signTransactionState ? JSON.stringify(signTransactionState, null, 2) : "undefined"}</pre>
+        </div>
       )}
     </div>
   );
